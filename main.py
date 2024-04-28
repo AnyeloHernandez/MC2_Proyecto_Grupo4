37ecb01ea0f6f02a6ce5d8e7f1c02d3b51791071
AndresJ2610 <140097501+AndresJ2610@users.noreply.github.com>
2024-04-28 11:22:43 -0600

    modificado

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import networkx as nx
 import tkinter as tk
+from tkinter import * 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+
 
 G = nx.Graph()
 root = tk.Tk()
 root.title("Algoritmo de busqueda de Anchura")
+root.configure(bg="lightblue")
 vertex_entry = tk.Entry(root)
 vertex_entry.pack()
-add_vertex_button=tk.Button(root, text="Agregar Vertice", command=lambda:G.add_node(vertex_entry.get()))
+photo = PhotoImage(file = "R.png")
+photoimage  = photo.subsample(3,3)
+lupa = PhotoImage(file = "lupa.png")
+lupaimage = lupa.subsample(3,3)
+eye = PhotoImage(file = "eye.png")
+eyeimage = eye.subsample(3,3)
+lapiz = PhotoImage(file = "lapiz.png")
+lapizimage = lapiz.subsample(3,3)
+add_vertex_button=tk.Button(root, text="Agregar Vertice", font=("Lucida Console", 10, "bold"), image=photoimage, compound=LEFT, background='Turquoise', activebackground='blue', activeforeground='white', command=lambda:G.add_node(vertex_entry.get()))
 add_vertex_button.pack()
 
 edge_entry_1 = tk.Entry(root)
 edge_entry_1.pack()
 edge_entry_2 = tk.Entry(root)
 edge_entry_2.pack()
-add_edge_button = tk.Button(root, text="Agregar Arista", command=lambda:G.add_edge(edge_entry_1.get(),edge_entry_2.get()))
+add_edge_button = tk.Button(root, text="Agregar Arista", font=("Lucida Console", 10, "bold"), image=photoimage, compound=LEFT, background='Turquoise', activebackground='blue', activeforeground='white', command=lambda:G.add_edge(edge_entry_1.get(),edge_entry_2.get()))
 add_edge_button.pack()
 
-print_info_button = tk.Button(root, text="Info de datos agregados (consola)", command=lambda:print("Número de nodos:", G.number_of_nodes(),"\nNumero de bordes:",G.number_of_edges()))
+print_info_button = tk.Button(root, text="Mostrar datos agregados (terminal)", font=("Lucida Console", 10, "bold"), image=eyeimage, compound=LEFT, background='Turquoise', activeforeground='white', activebackground='blue', command=lambda:print("Número de nodos:", G.number_of_nodes(),"\nNumero de bordes:",G.number_of_edges()))
 print_info_button.pack()
 
 figure = Figure(figsize=(5,5))
@@ -36,7 +47,7 @@ def draw_graph(bfs_edges=None):
         nx.draw(G, ax=ax, with_labels=True)
         canvas.draw
 
-draw_button = tk.Button(root, text="Dibujar grafo", command=draw_graph)
+draw_button = tk.Button(root, text="Dibujar grafo", font=("Lucida Console", 10, "bold"), image=lapizimage, compound=LEFT, background='Turquoise', activeforeground='white', activebackground='blue', command=draw_graph)
 draw_button.pack()
 
 def show_bfs():
@@ -44,6 +55,6 @@ def show_bfs():
     draw_graph(bfs_edges)
     canvas.draw()
 
-bfs_button = tk.Button(root, text="Busqueda de anchura", command=show_bfs)
+bfs_button = tk.Button(root, text="Busqueda de ancho", font=("Lucida Console", 10, "bold"), image=lupaimage, compound=LEFT, background='Turquoise', activeforeground='white', activebackground='blue', command=show_bfs)
 bfs_button.pack()
 root.mainloop()
